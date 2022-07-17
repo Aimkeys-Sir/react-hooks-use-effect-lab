c0491a04258aa69abd508a9c41906371634a619c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-17 16:44:07 +0300

    commit tree e4d0adfb842025943fcb9995b3b0149cf2a72ab0

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const timeId=setTimeout(()=>{
+      setTimeRemaining(timeRemaining=>timeRemaining -=1)
+    },1000)
+    return function cleanup(){
+      clearTimeout(timeId)
+    }
+  },[timeRemaining])
+  if(timeRemaining<1){
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
